b8fb3bd41bc9d2ad5856a48e1246387b213a07c9
Adriano Santos <adriano_angelo@live.com>
2023-02-03 19:12:37 -0300

    Componente Botão #01

---
+++ src/components/Button.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {
+    TouchableHighlight,
+    Text,
+    Dimensions,
+    StyleSheet
+} from 'react-native'
+
+export default props => {
+    return (
+        <TouchableHighlight onPress={props.onClick}>
+            <Text style={styles.button}>{props.label}</Text>
+        </TouchableHighlight>
+    )
+}
+
+const styles = StyleSheet.create({
+    button: {
+        fontSize: 40,
+        height: Dimensions.get('window').width / 4,
+        width: Dimensions.get('window').width / 4,
+        padding: 20,
+        backgroundColor: '#f0f0f0',
+        borderWidth: 1,
+        borderColor: '#888'
+    }
+})

b0a17de25d899bc19d86a34827f1448f99f4bd3c
Adriano Santos <adriano_angelo@live.com>
2023-02-03 18:47:54 -0300

    Iniciando projeto Calculator

---
+++ App.jsx
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react'
+import {
+  SafeAreaView,  
+  StyleSheet  
+} from 'react-native'
+
+function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+    container : {
+        flex: 1,
+        padding: 16
+    }
+})
+
+export default App;

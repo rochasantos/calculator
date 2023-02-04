de1c9aad1119b12f5b6c621998fe12ba97cd6ca8
Adriano Santos <adriano_angelo@live.com>
2023-02-04 10:42:18 -0300

    Logica calculadora #02 - método setOperation

--- App.jsx
+++ App.jsx
@@ -47,7 +47,26 @@ export default class App extends React.Component {
     }
 
     setOperation = operation => {
+        if(this.state.current === 0) 
+            this.setState({ operation, current: 1, clearDisplay: true })
+        else {
+            const equals = operation === '='
+            const values = [...this.state.values]
+            try {
+                values[0] = eval(`${values[0]} ${this.state.operation} ${values[1]}`)
+            } catch (e) {
+                values[0] = this.state.values[0]
+            }
 
+            values[1] = 0
+            this.setState({
+                displayValue: values[0],
+                operation: equals ? null : operation,
+                current: equals ? 0 : 1,
+                clearDisplay: !equals,
+                values,
+            })
+        }
     }
 
     render() {
@@ -56,7 +75,7 @@ export default class App extends React.Component {
                 <Display value={this.state.displayValue} />
                 <View style={styles.buttons}>
                     <Button label='AC' triple onClick={this.clearMemory}/>
-                    <Button label='/' operation/>
+                    <Button label='/' operation onClick={this.setOperation}/>
                     <Button label='7' onClick={this.addDigit} />
                     <Button label='8' onClick={this.addDigit} />
                     <Button label='9' onClick={this.addDigit} />
